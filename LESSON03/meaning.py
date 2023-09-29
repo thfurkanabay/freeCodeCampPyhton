96e945f5c01bbb652921c77d9fb416b2fca35684
thfurkanabay <thfurkanabay@gmail.com>
2023-09-29 09:30:48 +0100

    add data_types.py file

--- LESSON03/meaning.py
+++ LESSON03/meaning.py
@@ -1,7 +1,11 @@
 meaning = 42
 print('')
 
-if meaning > 10:
-    print('Right on!')
-else:
-    print('No today')
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('Not today!')
+
+# Ternitay Operator
+
+print("Right On!") if meaning > 10 else print('Not today')
---
+++ data_types.py
@@ -0,0 +1,10 @@
+# Strign data types
+
+# literal assignment
+
+first = "Furkan"
+last = "Abay"
+print(type(first))
+
+print(type(first) == str)
+print(isinstance(first, str))
